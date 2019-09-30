91b2f10c4ddd60a30615b30b7d631af12d672608
Carlos Saito <carlos@saito.style>
2019-09-30 15:23:15 +0200

    Use "skog" without Bunyan

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-process.emitWarning('There is nothing here. require "skog/bunyan" instead')
+const { createNamespace } = require('cls-hooked')
+const ns = createNamespace('skog')
+
+let defaultLogger = {
+  trace: (...args) => console.trace(...args),
+  debug: (...args) => console.debug(...args),
+  info: (...args) => console.info(...args),
+  warn: (...args) => console.warn(...args),
+  error: (...args) => console.error(...args),
+  fatal: (...args) => console.error(...args),
+  child: () => {}
+}
+
+function getCurrentLogger () {
+  return ns.get('logger') || defaultLogger
+}
+
+function setCurrentLogger (logger) {
+  if (ns.get('logger')) {
+    ns.set('logger', logger)
+  } else {
+    defaultLogger = logger
+  }
+}
+
+module.exports = {
+  child (options, callback) {
+    return ns.runAndReturn(async () => {
+      ns.set('logger', getCurrentLogger().child(options))
+      return callback()
+    })
+  },
+
+  get logger () {
+    return getCurrentLogger()
+  },
+
+  set logger (newLogger) {
+    return setCurrentLogger(newLogger)
+  },
+
+  trace: (...args) => getCurrentLogger().trace(...args),
+  debug: (...args) => getCurrentLogger().debug(...args),
+  info: (...args) => getCurrentLogger().info(...args),
+  warn: (...args) => getCurrentLogger().warn(...args),
+  error: (...args) => getCurrentLogger().error(...args),
+  fatal: (...args) => getCurrentLogger().fatal(...args)
+}
