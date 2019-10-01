0ce456d7926f7b0f0fe3ee6a7fed97d37c938d10
Carlos Saito <carlos@saito.style>
2019-10-01 09:28:08 +0200

    Simplified API for skog/bunyan

--- bunyan/index.js
+++ bunyan/index.js
@@ -1,33 +1,8 @@
 const bunyan = require('bunyan')
-const { createNamespace } = require('cls-hooked')
-const ns = createNamespace('skog/bunyan')
-
-let log
-
-function getCurrentLogger () {
-  return ns.get('logger') || log
-}
+const skog = require('../')
 
 module.exports = {
   createLogger (options) {
-    log = bunyan.createLogger(options)
-  },
-
-  child (options, callback) {
-    return ns.runAndReturn(async () => {
-      ns.set('logger', getCurrentLogger().child(options))
-      return callback()
-    })
-  },
-
-  get logger () {
-    return getCurrentLogger()
-  },
-
-  trace: (...args) => getCurrentLogger().trace(...args),
-  debug: (...args) => getCurrentLogger().debug(...args),
-  info: (...args) => getCurrentLogger().info(...args),
-  warn: (...args) => getCurrentLogger().warn(...args),
-  error: (...args) => getCurrentLogger().error(...args),
-  fatal: (...args) => getCurrentLogger().fatal(...args)
+    skog.logger = bunyan.createLogger(options)
+  }
 }
