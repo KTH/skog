643096cbf7a034531964bdf946033d4b3db5471b
jhsware <sebastian@urbantalk.se>
2022-03-02 09:30:55 +0100

    Use export * to combine exports when not cherry picking or renaming

--- packages/skog-core/src/index.ts
+++ packages/skog-core/src/index.ts
@@ -1,19 +1,2 @@
-import {
-  getFields,
-  setFields,
-  skogify,
-  runWithContext,
-  skogMiddleware,
-} from "./asyncContext";
-import { LogFunction, Logger, consoleLog } from "./log";
-
-export {
-  getFields,
-  setFields,
-  skogify,
-  skogMiddleware,
-  runWithContext,
-  LogFunction,
-  Logger,
-  consoleLog,
-};
+export * from "./asyncContext";
+export * from "./log";
