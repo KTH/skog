ad3947aa0151a5057653b579efe8a29faa334293
Carlos Saito <carlos@saito.style>
2022-03-29 13:11:16 +0200

    Use console or pino depending on initialization

--- packages/skog/src/index.ts
+++ packages/skog/src/index.ts
@@ -1,4 +1,4 @@
-import consoleLog from "skog-core";
+import consoleLog, { Levels, Logger } from "skog-core";
 import pinoLog, {
   initializeLogger as initializePinoLogger,
   PinoLoggerOptions,
@@ -16,7 +16,27 @@ export function initializeLogger(options: PinoLoggerOptions = {}) {
   });
   instance = pinoLog;
 }
-export default instance;
+
+function logFn(level: Levels, arg1: Error | string | object, arg2?: unknown) {
+  const fn = instance[level];
+
+  if (typeof arg1 === "string") {
+    fn(arg1);
+  } else if (typeof arg2 === "undefined" || typeof arg2 === "string") {
+    fn(arg1, arg2);
+  }
+}
+
+const logger: Logger = {
+  trace: (arg1, arg2?) => logFn("trace", arg1, arg2),
+  debug: (arg1, arg2?) => logFn("debug", arg1, arg2),
+  info: (arg1, arg2?) => logFn("info", arg1, arg2),
+  warn: (arg1, arg2?) => logFn("warn", arg1, arg2),
+  error: (arg1, arg2?) => logFn("error", arg1, arg2),
+  fatal: (arg1, arg2?) => logFn("fatal", arg1, arg2),
+};
+
+export default logger;
 export {
   addSkogContext,
   getFields,
